1fb2536fe2ab1a93b5faa7952af4f67ddfa8a6a4
sajadmhy <sajad.mahyaei@gmail.com>
2022-06-25 23:44:58 +0430

    Implement Auth0 into app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import User from './components/User';
+import './App.css'
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isAuthenticated ? (
+        <div>
+          <p style={{ fontSize: "1.5rem" }}>Please Login</p>
+           <LoginButton />
+        </div>
+      ) :
+        <div>
+            <LogoutButton />
+            <div className='space'></div>
+            <User />
+        </div>}
     </div>
   );
 }
---
+++ src/components/User.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import './User.css';
+
+export default function User() {
+    const { user, isAuthenticated } = useAuth0();
+
+  return (
+    isAuthenticated && (
+      <div className="User">
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
+  );
+}
